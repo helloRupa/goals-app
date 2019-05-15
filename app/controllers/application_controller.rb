943c13142e3a7046d98f23c815433ca6d27b31ed
Rupa Dhillon <rupa211984@gmail.com>
2019-05-14 22:56:35 -0700

    Test SessionsController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,25 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user
+
+  def current_user
+    @user = User.find_by_session_token(session[:session_token])
+  end
+
   def login_user!(user)
-    session_token = user.reset_session_token!
-    session[:session_token] = session_token
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def not_logged_in
+    return if logged_in?
+    redirect_to new_session_url
+  end
+
+  def already_logged_in
+    return unless logged_in?
+    redirect_to users_url
   end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,76 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe 'GET #new' do
+    it 'renders the new template successfully' do
+      get :new
+      expect(response).to have_http_status(200)
+      expect(response).to render_template(:new)
+    end
+  end
 
+  def login_user(username, password)
+    post :create, params: { user: { username: username, password: password } }
+  end
+
+  def save_valid_user
+    credentials = %w[Frankenstein password]
+    User.destroy_all
+    User.create(username: credentials[0], password: credentials[1])
+    credentials
+  end
+
+  describe 'POST #create' do
+    context 'with invalid credentials' do
+      before(:each) do
+        login_user('aaa', '1')
+      end
+
+      it 'does not set the session token cookie' do
+        expect(session[:session_token]).to be_nil
+      end
+
+      it 'renders the new template' do
+        expect(response).to render_template(:new)
+      end
+
+      it 'provides an error message' do
+        expect(flash[:errors]).to be_present
+      end
+    end
+
+    context 'with valid credentials' do
+      before(:each) do
+        username, password = save_valid_user
+        login_user(username, password)
+      end
+
+      it 'logs the user in' do
+        expect(session[:session_token]).to eq(User.first.session_token)
+      end
+
+      it 'redirects to the user page' do
+        expect(response).to have_http_status(302)
+        expect(response).to redirect_to(user_url(User.first.id))
+      end
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    before(:each) do
+      username, password = save_valid_user
+      login_user(username, password)
+      delete :destroy, params: { id: User.first.id }
+    end
+
+    it 'logs the user out' do
+      expect(session[:session_token]).to be_nil
+      expect(User.first.session_token).to_not eq(session[:session_token])
+    end
+
+    it 'redirects to the new session (login) page' do
+      expect(response).to have_http_status(302)
+      expect(response).to redirect_to(new_session_url)
+    end
+  end
 end
